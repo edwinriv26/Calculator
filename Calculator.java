885e69e371494d8deb8762bfd63155b019add332
edwinriv26 <edwin.rivera.410@my.csun.edu>
2024-01-29 16:56:46 -0800

    Added Option Lines

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,20 @@ public class Calculator {
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
         
-        System.out.println("Test");
+        while (true) {
+            System.out.println("---Calculator---")
+            System.out.println("1. Add");
+            System.out.println("2. Subtract");
+            System.out.println("3. Multiply");
+            System.out.println("4. Division");
+            System.out.println("5. Power X");
+            System.out.println("6. Root");
+            System.out.println("7. Percent");
+            System.out.println("8. Fraction");
+            System.out.println("9. Undo");
+            System.out.println("10. Clear");
+            System.out.println("11. History");
+            break;
+        }
     }
 }
