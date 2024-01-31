b8002a0856c9685a99c7b781b457b08e9261a32d
edwinriv26 <edwin.rivera.410@my.csun.edu>
2024-01-30 16:38:57 -0800

    Added to Loop

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 public class Calculator {
     
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
+        ArrayList<Double> numbers = new ArrayList<>();
+        ArrayList<String> operationlist = new ArrayList<>();
+        boolean isCalculating = true;
         
-        while (true) {
-            System.out.println("---Calculator---")
+        while (isCalculating) {
+            // Prompts 
+            System.out.println("---Calculator---");
             System.out.println("1. Add");
             System.out.println("2. Subtract");
             System.out.println("3. Multiply");
@@ -17,7 +23,36 @@ public class Calculator {
             System.out.println("9. Undo");
             System.out.println("10. Clear");
             System.out.println("11. History");
-            break;
+            System.out.println("----------------");
+            
+            
+                //User Input 
+                while (true) {
+                    System.out.print("Enter a number: ");
+                    while (!input.hasNextDouble()) {
+                        System.out.println("Please enter a valid number.");
+                        input.next(); // Consume the invalid input
+                    }
+                    double number = input.nextDouble();
+                    numbers.add(number);
+                
+                    System.out.print("Enter an operation (add, subtract, etc.) or '0' to solve: ");
+                    String operation = input.next();
+                    if (!operation.equals("0")){
+                    operationlist.add(operation);
+                    
+
+                
+                    if (operation.equals("0")) {
+                        isCalculating = false;
+                        break; //    Exit the loop to perform the final calculation
+                    }
+
+
+
+
+                }
         }
+        input.close();
     }
 }
